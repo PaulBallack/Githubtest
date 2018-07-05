ba2b6485a5737831ecc9acfea3f3c7050c69a779
Paul Ballack <32893660+PaulBallack@users.noreply.github.com>
2018-07-05 11:18:42 +0200

    string erstellt

--- app/src/main/java/com/example/paulb/githubtest2/MainActivity.java
+++ app/src/main/java/com/example/paulb/githubtest2/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String test = "test";
+    private String test = "testo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
